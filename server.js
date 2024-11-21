c6bdef9b5889f9e8c126f2f66b4041768ed1d522
RPTecDev <rrpgold@gmail.com>
2024-11-21 03:03:41 -0300

    modificações 2 json

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 import express from "express";
 
-const library = [
-    {
+const books = [
+    {id: 1,
       descricao: "Uma foto teste",
       imagem: "https://placecats.com/millie/300/150"
     },
-    {
+    {id: 2,
       descricao: "Gato preguiçoso tomando sol",
       imagem: "https://placecats.com/millie/300/150"
     },
-    {
+    {id: 3,
       descricao: "Gatinho brincando com um novelo de lã",
       imagem: "https://placecats.com/millie/300/150"
     },
-    {
+    {id: 4,
       descricao: "Visão de um gato curioso",
       imagem: "https://placecats.com/millie/300/150"
     },
-    {
+    {id: 5,
       descricao: "Gatos se divertindo em uma caixa de papelão",
       imagem: "https://placecats.com/millie/300/150"
     },
-    {
+    {id: 6,
       descricao: "Um gato com olhos esverdeados",
       imagem: "https://placecats.com/millie/300/150"
     }
@@ -36,5 +36,16 @@ app.listen(3000, () => {
 });
 
 app.get("/library", (req, res) => {
-    res.status(200).json(library);
+    res.status(200).json(books);
+});
+
+function buscarBookPorID(id) {
+    return books.findIndex((book) => {
+      return book.id === Number(id)
+    })
+};
+
+app.get("/library/:id", (req, res) => {
+    const index = buscarBookPorID(req.params.id)
+    res.status(200).json(books[index]);
 });
